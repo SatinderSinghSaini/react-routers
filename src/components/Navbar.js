eefdc05bd78685747f2839a86b85894c4c65f8fb
Satinder Singh Saini <swv095@gmail.com>
2021-08-15 12:24:39 +0530

    Used Link and NavLink instead of Anchor tag for routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
+import { Link, NavLink } from 'react-router-dom'
 let Navbar = ()=>{
     return (
         <div className="Navbar">
-            <span className="nav-item"><a href="/">Home</a></span>
-            <span className="nav-item"><a href="/about">About</a></span>
-            <span className="nav-item"><a href="/contact">Contact</a></span>            
+            <span className="nav-item"><Link to="/">Home</Link></span>
+            <span className="nav-item"><Link to="/about">About</Link></span>
+            <span className="nav-item"><NavLink to="/contact">Contact</NavLink></span>         
         </div>
     )
 }
